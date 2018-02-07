a5ddd86cc7c2bbae4787d8fd7bb40d65cbc97711
heystingray <austin.gray15@ncf.edu>
2018-02-07 13:37:34 -0500

    Learning JavaFX UI

--- src/HeronPaint.java
+++ src/HeronPaint.java
@@ -5,8 +5,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
 
 public class HeronPaint extends Application {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        Scene s = new Scene(root, 300, 300, Color.BLACK);
+
+        final Canvas canvas = new Canvas(250,250);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.BLUE);
+        gc.fillRect(75,75,100,100);
+
+        root.getChildren().add(canvas);
+        primaryStage.setScene(s);
+        primaryStage.show();
+    }
 }
