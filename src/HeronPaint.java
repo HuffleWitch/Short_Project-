417211f37555222716fa8571bbfd86375c26895d
Austin Gray <austin.gray15@ncf.edu>
2018-02-08 23:25:32 -0500

    Most of the buttons added!

--- src/HeronPaint.java
+++ src/HeronPaint.java
@@ -10,6 +10,8 @@ import javafx.scene.paint.*;
 import javafx.scene.canvas.*;
 import javafx.scene.control.*;
 import javafx.geometry.Orientation;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.shape.Rectangle;
 
 public class HeronPaint extends Application {
     public static void main(String[] args) {
@@ -20,43 +22,68 @@ public class HeronPaint extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("HeronPaint");
         BorderPane root = new BorderPane();
-        Scene s = new Scene(root, 500, 500, Color.WHITE);
+        Scene s = new Scene(root, 1000, 800);//, Color.WHITE);
 
-        final Canvas canvas = new Canvas();
+        final Canvas canvas = new Canvas(1000,800);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         gc.setFill(Color.BLUE);
-        gc.fillRect(-25,75,700,100);
+        gc.fillRect(0,0,100,100);
 
-        HBox toolBar = new HBox(
-            new Button("New"),
-            new Button("Open"),
-            new Button("Save"),
-            new Separator(Orientation.VERTICAL),
-            new Button("Clean"),
-            new Button("Compile"),
-            new Button("Run"),
-            new Separator(Orientation.VERTICAL),
-            new Button("Debug"),
-            new Button("Profile")
-        );
+        // All the color buttons
+        Button whiteButton = new Button("", new Rectangle(20, 20, Color.WHITE));
+        Button redButton = new Button("", new Rectangle(20, 20, Color.RED));
+        Button yellowButton = new Button("", new Rectangle(20, 20, Color.YELLOW));
+        Button blueButton = new Button("", new Rectangle(20, 20, Color.BLUE));
+        Button blackButton = new Button("", new Rectangle(20, 20, Color.BLACK));
+        Button orangeButton = new Button("", new Rectangle(20, 20, Color.ORANGE));
+        Button greenButton = new Button("", new Rectangle(20, 20, Color.GREEN));
+        Button purpleButton = new Button("", new Rectangle(20, 20, Color.PURPLE));
 
-        HBox toolBar2 = new HBox(
-            new Button("New2"),
-            new Button("Open"),
-            new Button("Save"),
-            new Separator(Orientation.VERTICAL),
-            new Button("Clean"),
-            new Button("Compile"),
-            new Button("Run"),
-            new Separator(Orientation.VERTICAL),
-            new Button("Debug"),
-            new Button("Profile")
-        );
+        // All the other buttons
+        Button penButton = new Button("PEN");
+        Button eraseButton = new Button("ERASE");
+        Button selectButton = new Button("SELECT");
+        Button rectButton = new Button("RECTANGLE");
+        Button circButton = new Button("CIRCLE");
+        Button polyButton = new Button("POLYGON");
+        Button textButton = new Button("TEXT");
+        Button noteButton = new Button("NOTE");
+        Button hideButton = new Button("HIDE NOTES");
+        Button importButton = new Button("IMPORT");
+        Button saveButton = new Button("SAVE");
+        Button loadButton = new Button("LOAD");
+        Button printButton = new Button("PRINT");
+
+        ButtonBar topButtons = new ButtonBar();
+
+        topButtons.getButtons().addAll(whiteButton,
+                                    redButton,
+                                    yellowButton,
+                                    blueButton,
+                                    penButton,
+                                    selectButton,
+                                    polyButton,
+                                    textButton,
+                                    importButton,
+                                    saveButton,
+                                    printButton);
+
+        ButtonBar botButtons = new ButtonBar();
+
+        botButtons.getButtons().addAll(blackButton,
+                                     orangeButton,
+                                     greenButton,
+                                     purpleButton,
+                                     eraseButton,
+                                     rectButton,
+                                     circButton,
+                                     noteButton,
+                                     hideButton,
+                                     loadButton);
 
         VBox vbox = new VBox();
-        vbox.getChildren().add(toolBar);
-        vbox.getChildren().add(toolBar2);
+        vbox.getChildren().addAll(topButtons, botButtons);
 
         root.setCenter(canvas);
         root.setTop(vbox);
