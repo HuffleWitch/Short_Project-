9ff336ac5747f9fd170c4f054bc0cc19588e115a
Austin Gray <austin.gray15@ncf.edu>
2018-02-16 02:20:23 -0500

    Updated Shape interface for movement. All the shapes are wrong now! Huzzah!

--- src/Shape.java
+++ src/Shape.java
@@ -1,7 +1,8 @@
 
 
 
-public interface Shape
-{
+public interface Shape {
     public void draw();
+    public boolean isInHitBox(double isX, double isY);
+    public void move(double deltaX, double deltaY);
 }
